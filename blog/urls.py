973b3b6653beb3bed71b65b73f5ddeddd37e90a4
Cho Seonghan <chosnhn1@gmail.com>
2025-05-05 17:05:40 +0900

    add comments

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
 
     # post_share
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+
+    # comment form
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
+import os
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-import os
-from .models import Post
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 # function-based view
 # def post_list(request):
@@ -47,7 +48,10 @@ def post_detail(request, year, month, day, post):
     # except Post.DoesNotExist:
     #     raise Http404("No Post found.")
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
@@ -68,3 +72,17 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # set post into comment, with ModelForm's save method
+        # notice that Form cannot perform this
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
